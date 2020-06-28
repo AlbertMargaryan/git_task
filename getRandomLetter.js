3ed05affd501a806ef5c82227ba866647d51c1a4
Albert Margaryan <albertmargaryan2004@gmail.com>
2020-06-28 14:16:26 +0400

    added some examples

--- getRandomLetter.js
+++ getRandomLetter.js
@@ -1,3 +1,7 @@
 function getRandomNumber(num = 2){
 	return Math.round(Math.random() * num);
 }
+console.log(getRandomNumber())
+console.log(getRandomNumber(20))
+console.log(getRandomNumber(20))
+console.log(getRandomNumber(5153))
--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,6 +1,9 @@
-
 function getRandomLetter(){
 	let n = "abcdefghijklmnopqrstuvwxyz";
   let x = Math.round(Math.random() * n.length)
 	return n[x];
 }
+console.log(getRandomLetter())
+console.log(getRandomLetter())
+console.log(getRandomLetter())
+console.log(getRandomLetter())
